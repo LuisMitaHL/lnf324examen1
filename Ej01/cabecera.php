17fd155df04d72f8f8e24ddfe67579c8c4bcf02b
Luis Mita <luis@luismita.com>
2024-05-08 13:58:04 -0400

    Mejora de estilo 1

--- Ej01/cabecera.php
+++ Ej01/cabecera.php
@@ -10,8 +10,31 @@
             color: navy;
             margin-left: 20px;
         }
+        nav {
+            padding: 10px;
+            width: 100%;
+            display: block;
+        }
+        nav ul {
+            list-style-type: none;
+            margin: 0;
+            padding: 0;
+        }
+        nav ul li {
+            display: inline;
+            margin-right: 10px;
+        }
     </style>
 </head>
 <body>
+    <nav>
+        <ul>
+            <li><a href="#">Inicio</a></li>
+            <li><a href="#productos">Productos</a></li>
+            <li><a href="#servicios">Servicios</a></li>
+            <li><a href="#contacto">Contacto</a></li>
+        </ul>
+    </nav>
+
     <h1>Banco de la Fortuna</h1>
     
--- Ej01/index.php
+++ Ej01/index.php
@@ -1,11 +1,15 @@
 <?php
 include 'cabecera.php';
 ?>
+    <h2>Productos</h2>
     <p>En este banco le ofrecemos tres tipos de cuentas bancarias:</p>
     <ul>
         <li><strong>Caja de Ahorro:</strong> Este tipo de cuenta le permite ahorrar y ganar interés sobre el monto acumulado, para dinero que no se usará de inmediato.</li>
         <li><strong>Cuenta Corriente:</strong> Esta cuenta le permite hacer transacciones frecuentes.</li>
         <li><strong>DPF (Depósito a Plazo Fijo):</strong> Ofrecemos la tasa de interés más alta, si Ud se compromete a no retirar el dinero en cierto tiempo especificado.</li>
+    </ul>
+    <h2>Contacto</h2>
+    <p>Si tienes alguna consulta, visita nuestras oficinas en la Agencia Principal <strong>Av. Busch #9876</strong></p>
     </ul>
     <?php
 include 'pie.php';
